1297709c4f4aee0f069d6ce5c8935265a51da98b
Jesus Zapata <chuchocorleone@gmail.com>
2016-11-11 08:30:02 -0400

    Modifying SQL to meet the challenge py1

--- primes.py
+++ primes.py
@@ -1,16 +1,21 @@
 """
-Your module documentation here
+Module that calculates whether a number is prime or not
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Class that calculates whether an integer is prime or not
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Method that receives an integer and says if it is prime
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int <= 1:
+            return False
+        else:
+            for divisor in xrange(2, num_int):
+                if num_int % divisor == 0:
+                    return False
+            return True
